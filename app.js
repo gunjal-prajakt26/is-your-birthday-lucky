49503867e2f9be991da688b68b8ad04b8ae9e407
gunjal-prajakt26 <gunjal.prajakt951@gmail.com>
2022-01-29 13:33:36 +0530

    style updated

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var luckyNumber = document.querySelector(".lucky-number-input");
 var checkBtn = document.querySelector(".check-btn");
 var ouput =document.querySelector(".output-div");
 
-
 checkBtn.addEventListener('click', function getvalues(){
     let dob = birthdate.value.replaceAll("-","");
     let luck = luckyNumber.value;
@@ -12,17 +11,16 @@ checkBtn.addEventListener('click', function getvalues(){
     if(sum&&luck){
     compareValues(sum, luck);
     }else{
-        ouput.innerText="Enter Both Feilds";
+        ouput.innerText="Enter Both Feilds please 😡";
     }
 });
 
 function compareValues( total, number){
     if(total % number===0){
-        console.log("hi");
-        ouput.innerText="Yehh! Your Birthday is lucky!";
+        ouput.innerText="Yehh! Your Birthday is lucky! 🥳";
     }else{
 
-        ouput.innerText="Sorry your birthday is not lucky. So sad!";
+        ouput.innerText="Sorry your birthday is not lucky. So sad! 🙃";
     }
 }
 
@@ -30,7 +28,7 @@ function compareValues( total, number){
 function calculateSum(date){
     let sum = 0;
     for(var i=0; i<date.length; i++){
-        sum = sum + Number(date.charAt(i)); 
+        sum = sum + Number(date.charAt(i));
     }
     return sum;
 }
